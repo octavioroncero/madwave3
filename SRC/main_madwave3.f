61ed7e01a70fc38a30f3ea3e8adc8d21bc71196a
octavio <octavio.roncero@csic.es>
2021-06-29 13:18:52 +0200

    stop with norm problem solved in main_mad3wave.f

--- SRC/main_madwave3.f
+++ SRC/main_madwave3.f
@@ -396,7 +396,8 @@ c! the partition.
 
       call MPI_REDUCE(xnorm1,xnorm1tot,1,MPI_REAL8,MPI_SUM
      &                             ,0,MPI_COMM_WORLD,ierr)
-      if(idproc.eq.0)then
+      call MPI_BCAST(xnorm1tot,1,MPI_REAL8,0,MPI_COMM_WORLD,ierr)
+!      if(idproc.eq.0)then
          write(6,*)it,xnorm1tot
       endif
 
