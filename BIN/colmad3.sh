c5354565ad8b0620e4f4e01de5257d9a55a7abd9
octavio <octavio.roncero@csic.es>
2026-02-11 18:20:34 +0100

    cambio en colmad3.sh shell

--- BIN/colmad3.sh
+++ BIN/colmad3.sh
@@ -1,27 +1,33 @@
-dir=/work/octavio/madwave3/photoBC/SRC
+dir=../SRC
 dir2=$dir/AUXILIARy-CODES
 dir3=$dir/bound-lanz
 dir4=$dir/photo
 
 pot=../SRC
 
+module load  gcc/system openmpi/4.1.4_ft3
+module load  fftw/3.3.10
+
+FCFLAGS="-DUSE_MPIF -fallow-invalid-boz -fallow-argument-mismatch"
+
+
 ######################  compiling modules
 
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_gridYpara_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_pot_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_baseYfunciones_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_Hphi_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_photoini_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_colini_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_absorcion_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_flux_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_coortrans01_02.f
-      mpif77 -c  -g -C -fcheck=all  $dir3/mod_lanczos_01y2.f
-      mpif77 -c  -g -C -fcheck=all  $dir/mod_PsiE_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_gridYpara_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_pot_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_baseYfunciones_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_Hphi_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_photoini_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_colini_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_absorcion_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_flux_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_coortrans01_02.f
+      mpif77 -c  -O3 $FCFLAGS  $dir3/mod_lanczos_01y2.f
+      mpif77 -c  -O3 $FCFLAGS  $dir/mod_PsiE_01y2.f
 ######################  mad3.out
 rm mad3.out
 
-mpif77 -g -C -fcheck=all -o mad3.out  mod_colini_01y2.o  mod_Hphi_01y2.o mod_gridYpara_01y2.o\
+mpif77 -O3 $FCFLAGS -o mad3.out  mod_colini_01y2.o  mod_Hphi_01y2.o mod_gridYpara_01y2.o\
        mod_pot_01y2.o mod_baseYfunciones_01y2.o mod_photoini_01y2.o \
        mod_absorcion_01y2.o mod_flux_01y2.o mod_coortrans01_02.o mod_PsiE_01y2.o\
        $dir/main_madwave3.f $dir/liboctdyn.f \
@@ -29,7 +35,7 @@ mpif77 -g -C -fcheck=all -o mad3.out  mod_colini_01y2.o  mod_Hphi_01y2.o mod_gri
  -lfftw3
 ######################  mad3.out
 rm bndgrid.out 
-mpif77 -g -C -fcheck=all  -o bndgrid.out  mod_Hphi_01y2.o mod_gridYpara_01y2.o\
+mpif77 -O3 $FCFLAGS  -o bndgrid.out  mod_Hphi_01y2.o mod_gridYpara_01y2.o\
        mod_pot_01y2.o mod_baseYfunciones_01y2.o mod_photoini_01y2.o\
        mod_absorcion_01y2.o mod_lanczos_01y2.o\
        $dir3/main_boundlanz.f $dir/liboctdyn.f $dir3/liboctdynlanz.f \
@@ -38,30 +44,30 @@ mpif77 -g -C -fcheck=all  -o bndgrid.out  mod_Hphi_01y2.o mod_gridYpara_01y2.o\
 
 ######################  distri.out & distriREAC.out
 rm distri.out distriREAC.out
-gfortran  -g -C -fcheck=all -o distri.out $dir2/distriwvp.f $dir/liboctdyn.f
-gfortran  -g -C -fcheck=all -o distriREAC.out $dir2/distriREACwvp.f $dir/liboctdyn.f
+gfortran  -O3 $FCFLAGS -o distri.out $dir2/distriwvp.f $dir/liboctdyn.f
+gfortran  -O3 $FCFLAGS -o distriREAC.out $dir2/distriREACwvp.f $dir/liboctdyn.f
 
 ######################  crp.out & cip.out & cipave.out
 rm cip.out crp.out cipave.out
-gfortran  -g -C -fcheck=all -o crp.out $dir2/CRP-fast.f $dir/liboctdyn.f
-gfortran  -g -C -fcheck=all -o cip.out $dir2/CIP-fast.f $dir/liboctdyn.f
+gfortran  -O3 $FCFLAGS -o crp.out $dir2/CRP-fast.f $dir/liboctdyn.f
+gfortran  -O3 $FCFLAGS -o cip.out $dir2/CIP-fast.f $dir/liboctdyn.f
 
 ######################  rate.out & rates2s.out
 rm rate.out rates2s.out
-gfortran  -g -C -fcheck=all -o rate.out $dir2/rateFromSigma.f $dir/liboctdyn.f
-gfortran  -g -C -fcheck=all -o rates2s.out $dir2/rate-s2s-fromCRP-extrapolation.f
-gfortran  -g -C -fcheck=all -o inelastic-rates2s.out $dir2/rate-s2s-fromCIP-extrapolation.f
+gfortran  -O3 $FCFLAGS -o rate.out $dir2/rateFromSigma.f $dir/liboctdyn.f
+gfortran  -O3 $FCFLAGS -o rates2s.out $dir2/rate-s2s-fromCRP-extrapolation.f
+gfortran  -O3 $FCFLAGS -o inelastic-rates2s.out $dir2/rate-s2s-fromCIP-extrapolation.f
 
 ######################  sigma.out 
 rm sigma.out
-mpif77  -g -C -fcheck=all -o sigma.out $dir2/sigmaFromS2prod.f $dir/liboctdyn.f
+mpif77  -O3 $FCFLAGS -o sigma.out $dir2/sigmaFromS2prod.f $dir/liboctdyn.f
 ######################  cheby-spectra.out 
 rm cheby-spectra.out
-gfortran  -g -C -fcheck=all -o cheby-spectra.out $dir4/cheby-spectra.f 
+gfortran  -O3 $FCFLAGS -o cheby-spectra.out $dir4/cheby-spectra.f 
 
 ######################  Einstein.out
 rm Einstein.out
-mpif77 -o Einstein.out -g -C -fcheck=all  mod_gridYpara_01y2.o mod_baseYfunciones_01y2.o mod_pot_01y2.o mod_Hphi_01y2.o mod_photoini_01y2.o $photo/Acoefficient-bound-bound.f $dir/liboctdyn.f $dir/fit_general.f $dir/dipele_general.f $dir/coupling_general.f -lfftw3
+mpif77 -o Einstein.out -O3 $FCFLAGS  mod_gridYpara_01y2.o mod_baseYfunciones_01y2.o mod_pot_01y2.o mod_Hphi_01y2.o mod_photoini_01y2.o $photo/Acoefficient-bound-bound.f $dir/liboctdyn.f $dir/fit_general.f $dir/dipele_general.f $dir/coupling_general.f -lfftw3
 
 
 echo "12 executable codes: mad3.out bndgrid.out Einstein.out distri.out  distriREAC.out  crp.out  cip.out inelastic-rates2s.out rate.out rates2s.out sigma.out  cheby-spectra.out"
